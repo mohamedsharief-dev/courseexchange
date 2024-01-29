d64fcbe4f388753cee89ae9c1520278caec742eb
Moe Sharief <hello@moe-sharief.dev>
2024-01-29 15:21:13 +0300

    Update nexusfunc.js refactored results counter

--- nexusfunc.js
+++ nexusfunc.js
@@ -61,9 +61,9 @@ event.preventDefault();
 
   // Reset filteredCourses to the entire set
   state.filteredCourses = [...state.querySet]; 
+  updateResultsCounter(state.filteredCourses);
   const allSubcategories = uniqueValues(state.querySet, 'courseSubCategory');
-  populateCheckboxFilter('subcategory-filter', allSubcategories, true);
-  updateResultsCounter(state.filteredCourses); 
+  populateCheckboxFilter('subcategory-filter', allSubcategories, true); 
   displayCourses(state.filteredCourses, 1, state.rows);
 }
 
